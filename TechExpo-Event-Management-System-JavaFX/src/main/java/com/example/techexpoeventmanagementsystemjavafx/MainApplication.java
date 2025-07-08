5f7ad252e5424c5cf9b9978e50997783e2082b81
zurii-07 <surakkithag@gmail.com>
2025-07-08 09:02:41 +0530

    Designed the main GUI

--- TechExpo-Event-Management-System-JavaFX/src/main/java/com/example/techexpoeventmanagementsystemjavafx/MainApplication.java
+++ TechExpo-Event-Management-System-JavaFX/src/main/java/com/example/techexpoeventmanagementsystemjavafx/MainApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("Main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 760, 598);
+        stage.setTitle("TechExpo Event Management System");
         stage.setScene(scene);
         stage.show();
     }
--- TechExpo-Event-Management-System-JavaFX/src/main/java/com/example/techexpoeventmanagementsystemjavafx/MainController.java
+++ TechExpo-Event-Management-System-JavaFX/src/main/java/com/example/techexpoeventmanagementsystemjavafx/MainController.java
@@ -1,14 +1,45 @@
 package com.example.techexpoeventmanagementsystemjavafx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.util.List;
+import javafx.application.Platform;
 
 public class MainController {
+    //private final ProjectManager projectManager = new ProjectManager(); // Dependency injection of Project Manager object.
+
+    //Controls for buttons that are defined in FXMLs
     @FXML
-    private Label welcomeText;
+    private Button AddProjectDetailsButton;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Button UpdateProjectDetailsButton;
+
+    @FXML
+    private Button DeleteProjectDetailsButton;
+
+    @FXML
+    private Button ViewProjectDetailsButton;
+
+    @FXML
+    private Button SaveProjectDetailsButton;
+
+    @FXML
+    private Button RandomSpotlightSelectionButton;
+
+    @FXML
+    private Button AwardWinningProjectsButton;
+
+    @FXML
+    private Button VisualizeAwardWinningProjectsButton;
+
+    @FXML
+    private Button ExitButton;
+
 }
